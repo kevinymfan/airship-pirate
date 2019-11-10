e67ee29137660acf3bd73ce86cdbd2e2d2beb4aa
mitchkm <mitchkm21@gmail.com>
2019-11-09 22:54:44 -0800

    adds basic item fishing

---
+++ Assets/Scripts/FishPool.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishPool : MonoBehaviour
+{
+    [SerializeField]
+    private ItemSO[] firstFish;
+    private Queue<ItemSO> firstFishes;
+    [SerializeField]
+    private ItemSO[] pool;
+
+    [HideInInspector]
+    public ItemSO fishedItem;
+
+    private void Start() {
+        this.firstFishes = new Queue<ItemSO>(this.firstFish);
+    }
+
+    public ItemSO catchItem() {
+        ItemSO item = this.fishedItem;
+        this.fishedItem = null;
+        return item;
+    }
+
+    public void releaseItem() {
+        this.fishedItem = null;
+    }
+
+    public ItemSO fishItem() {
+        if (this.fishedItem) { }
+        else if (this.firstFishes.Count > 0) {
+            this.fishedItem = this.firstFishes.Dequeue();
+        }
+        else {
+            this.fishedItem = this.getRandomItem();
+        }
+        return this.fishedItem;
+    }
+
+    private ItemSO getRandomItem() {
+        ItemSO item = ScriptableObject.CreateInstance<ItemSO>();
+        return item;
+    }
+}
---
+++ Assets/Scripts/ItemSO.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "MyItem", menuName = "ScriptableObjects/Item", order = 1)]
+public class ItemSO : ScriptableObject
+{
+    public string itemName;
+    public string flavorText;
+    public float weight;
+
+    public override string ToString()
+    {
+        return string.Format("{0} ({1}kg): \"{2}\"", itemName, weight, flavorText);
+    }
+}
+
